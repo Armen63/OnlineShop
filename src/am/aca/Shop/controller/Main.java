beee619c558395f06ee3009aac41ddd238b17c80
Armen63 <Yamnchik7@mail.ru>
2016-09-05 19:37:04 +0400

    Mmmmmdaaa

---
+++ src/am/aca/Shop/controller/Main.java
@@ -0,0 +1,11 @@
+package am.aca.Shop.controller;
+
+/**
+ * Created by Armen on 9/5/2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Controller start = new Controller();
+        start.executor();
+    }
+}
